dff7efb481c55fe0eae0cf4753f6e51594d1682b
Ashar <asharrahman2002@gmail.com>
2023-12-18 02:09:02 -0500

    Texturing for block 2 is done, and program is effecient now!

--- main.js
+++ main.js
@@ -8,18 +8,171 @@ let CURRENT_SHAPE = "CUBE";
 let subdivisions1 = 1;
 let subdivisions2 = 1;
 
+let image;
+let gl;
+let canvas;
+let brickTexture;
+let thirdTexture;
+
+let textureLoaded = false;
 
 
-function draw() {
-	// Retrieve the canvas
-	const canvas = document.getElementById('webgl-canvas');
-	if (!canvas) {
-		console.error(`There is no canvas with id ${'webgl-canvas'} on this page.`);
-		return null;
-	}
 
-	const mat4 = glMatrix.mat4;
-	const gl = canvas.getContext('webgl2');
+const cube2Data = [
+    // Front
+    2.5, 0.5, 0.5, // top right
+    2.5, -.5, 0.5, // bottom right
+    1.5, 0.5, 0.5, // top left
+    1.5, 0.5, 0.5, // top left
+    2.5, -.5, 0.5, // bottom right
+    1.5, -.5, 0.5, // bottom left
+
+    // Left
+    1.5, 0.5, 0.5,
+    1.5, -.5, 0.5,
+    1.5, 0.5, -.5,
+    1.5, 0.5, -.5,
+    1.5, -.5, 0.5,
+    1.5, -.5, -.5,
+
+    // Back
+    1.5, 0.5, -.5,
+    1.5, -.5, -.5,
+    2.5, 0.5, -.5,
+    2.5, 0.5, -.5,
+    1.5, -.5, -.5,
+    2.5, -.5, -.5,
+
+    // Right
+    2.5, 0.5, -.5,
+    2.5, -.5, -.5,
+    2.5, 0.5, 0.5,
+    2.5, 0.5, 0.5,
+    2.5, -.5, 0.5,
+    2.5, -.5, -.5,
+
+    // Top
+    2.5, 0.5, 0.5,
+    2.5, 0.5, -.5,
+    1.5, 0.5, 0.5,
+    1.5, 0.5, 0.5,
+    2.5, 0.5, -.5,
+    1.5, 0.5, -.5,
+
+    // Underside
+    2.5, -.5, 0.5,
+    2.5, -.5, -.5,
+    1.5, -.5, 0.5,
+    1.5, -.5, 0.5,
+    2.5, -.5, -.5,
+    1.5, -.5, -.5,
+];
+
+const cube3Data = [
+    // Front
+    -0.5, 0.5, 0.5, // top right
+    -0.5, -.5, 0.5, // bottom right
+    -1.5, 0.5, 0.5, // top left
+    -1.5, 0.5, 0.5, // top left
+    -0.5, -.5, 0.5, // bottom right
+    -1.5, -.5, 0.5, // bottom left
+
+    // Left
+    -1.5, 0.5, 0.5,
+    -1.5, -.5, 0.5,
+    -1.5, 0.5, -.5,
+    -1.5, 0.5, -.5,
+    -1.5, -.5, 0.5,
+    -1.5, -.5, -.5,
+
+    // Back
+    -1.5, 0.5, -.5,
+    -1.5, -.5, -.5,
+    -0.5, 0.5, -.5,
+    -0.5, 0.5, -.5,
+    -1.5, -.5, -.5,
+    -0.5, -.5, -.5,
+
+    // Right
+    -0.5, 0.5, -.5,
+    -0.5, -.5, -.5,
+    -0.5, 0.5, 0.5,
+    -0.5, 0.5, 0.5,
+    -0.5, -.5, 0.5,
+    -0.5, -.5, -.5,
+
+    // Top
+    -0.5, 0.5, 0.5,
+    -0.5, 0.5, -.5,
+    -1.5, 0.5, 0.5,
+    -1.5, 0.5, 0.5,
+    -0.5, 0.5, -.5,
+    -1.5, 0.5, -.5,
+
+    // Underside
+    -0.5, -.5, 0.5,
+    -0.5, -.5, -.5,
+    -1.5, -.5, 0.5,
+    -1.5, -.5, 0.5,
+    -0.5, -.5, -.5,
+    -1.5, -.5, -.5,
+];
+
+
+function repeat(n, pattern) {
+    return [...Array(n)].reduce(sum => sum.concat(pattern), []);
+}
+
+const uvData = repeat(6, [
+    1, 0, // top right
+    1, 1, // bottom right
+    0, 0, // top left
+    0, 0, // top left
+    1, 1, // bottom right
+    0, 1  // bottom left
+]);
+
+
+function draw(gl, canvas, brickTexture) {
+
+    const mat4 = glMatrix.mat4;
+
+
+    const brickFragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(brickFragmentShader, `
+    precision mediump float;
+    varying vec2 vUV;
+    uniform sampler2D uTexture;
+
+    void main() {
+        gl_FragColor = texture2D(uTexture, vUV);
+    }
+`);
+    gl.compileShader(brickFragmentShader);
+
+    const brickVertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(brickVertexShader, `
+    attribute vec3 vertexPosition;
+    attribute vec2 uv;
+    varying vec2 vUV;
+
+    uniform mat4 matrix;
+
+    void main() {
+        vUV = uv;
+        gl_Position = matrix * vec4(vertexPosition, 1.0);
+    }
+`);
+    gl.compileShader(brickVertexShader);
+
+    const brickProgram = gl.createProgram();
+    gl.attachShader(brickProgram, brickVertexShader);
+    gl.attachShader(brickProgram, brickFragmentShader);
+    gl.linkProgram(brickProgram);
+
+    const cube2Buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cube2Buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(cube2Data), gl.STATIC_DRAW);
 
 
 
@@ -29,9 +182,6 @@ function draw() {
 	// 	0, 1, 0,
 	// 	0, 0, 1
 	// ]
-
-
-
 
 	const positionBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
@@ -151,15 +301,73 @@ function draw() {
 
 	gl.drawArrays(gl.TRIANGLES, 0, vertexData.length / 3);
 
+    gl.useProgram(brickProgram);
+    const secondPositionLocation = gl.getAttribLocation(brickProgram, 'vertexPosition');
+    gl.enableVertexAttribArray(secondPositionLocation);
+    gl.bindBuffer(gl.ARRAY_BUFFER, cube2Buffer);
+    gl.vertexAttribPointer(secondPositionLocation, 3, gl.FLOAT, false, 0, 0);
+
+    const uvBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uvData), gl.STATIC_DRAW);
+
+    const uvLocation = gl.getAttribLocation(brickProgram, 'uv');
+    gl.enableVertexAttribArray(uvLocation);
+    gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
+    gl.vertexAttribPointer(uvLocation, 2, gl.FLOAT, false, 0, 0);
+
+    const textureLocation = gl.getUniformLocation(brickProgram, 'uTexture');
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, brickTexture);
+    gl.uniform1i(textureLocation, 0);
+
+    const matrixLocation = gl.getUniformLocation(brickProgram, 'matrix');
+    gl.uniformMatrix4fv(matrixLocation, false, mvpMatrix);
+
+    gl.drawArrays(gl.TRIANGLES, 0, cube2Data.length / 3);
+
+    console.log("draw called", gl.getError());
+
+}
+
+function loadTexture(url, gl) {
+    const texture = gl.createTexture();
+
+
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+    gl.generateMipmap(gl.TEXTURE_2D);
+
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+
+    console.log('Image width:', image.width);
+    console.log('Image height:', image.height);
+
+
+    return texture;
 }
 
 function init() {
+
+    // Retrieve the canvas
+     canvas = document.getElementById('webgl-canvas');
+    if (!canvas) {
+        console.error(`There is no canvas with id ${'webgl-canvas'} on this page.`);
+        return null;
+    }
+
+    gl = canvas.getContext('webgl2');
 	console.log("init called");
 	// animate();
 	window.addEventListener('keydown', gotKey ,false);
 
 	makeCube(subdivisions1);
-	draw();
+
+
+    brickTexture = loadTexture('brick.jpg', gl);
+	draw(gl, canvas, brickTexture);
 
 }
 
